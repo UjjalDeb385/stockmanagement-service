533d6e0f2006cc5e1da2ebfde7bc92af564b69af
ujjal <debujjal4@gmail.com>
2025-09-06 17:49:30 +0530

    add files

---
+++ src/main/java/com/stockpulse/stockpulse_service/StockpulseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stockpulse.stockpulse_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpulseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpulseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockpulse/stockpulse_service/StockpulseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpulse.stockpulse_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpulseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
